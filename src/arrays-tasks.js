6ed09a0ea658c5a7b1eaeeab259b714055d1bf50
ikclouds <ikclouds91@gmail.com>
2025-10-16 11:14:07 +0300

    feat: createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -553,8 +553,18 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  if (!Array.isArray(arr)) {
+    throw new Error('First argument must be an array');
+  }
+  if (typeof chunkSize !== 'number' || chunkSize <= 0) {
+    throw new Error('Chunk size must be a positive number');
+  }
+
+  const numChunks = Math.ceil(arr.length / chunkSize);
+  return Array.from({ length: numChunks }, (_, i) =>
+    arr.slice(i * chunkSize, i * chunkSize + chunkSize)
+  );
 }
 
 /**
